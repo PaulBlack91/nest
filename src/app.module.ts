1895bf20d87f76802614c9e3765eaaa14d760b04
Paul <paul.esperonarg@hotmail.com>
2024-07-05 18:30:48 -0300

    se agregan controladores

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,13 @@ import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
+import { TasksController } from './tasks/tasks.controller';
+import { UsersController } from './users/users.controller';
+import { ProjectsController } from './projects/projects.controller';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [TasksModule, AuthModule, ProjectsModule, UsersModule],
+  controllers: [TasksController, UsersController, ProjectsController, AuthController],
 })
 export class AppModule { }
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from "@nestjs/common";
+
+
+@Controller({})
+
+export class TasksController {
+
+  @Get('/tasks')
+  getAllTasks() {
+    return 'All tasks';
+  }
+    
+}
