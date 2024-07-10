c94cb30b5c1c77666036f2618fbb6a20b4950541
Paul Esperon <paul.esperonarg@hotmail.com>
2024-07-09 22:16:29 -0300

    add CRUD operations for tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,17 +1,36 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get, Patch, Post, Put } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 
 
-@Controller({})
+@Controller('/tasks')
 
 export class TasksController {
 
   constructor(private tasksService: TasksService) { }
 
-  @Get('/tasks')
+  @Get()
   getAllTasks() {
     return this.tasksService.getTasks();
   }
 
+  @Post()
+  postTask() {
+    return this.tasksService.postTask();
+  }
+
+  @Put()
+  putTask() {
+    return this.tasksService.putTask();
+  }
+
+  @Delete()
+  deleteTask() {
+    return this.tasksService.deleteTask();
+  }
+
+  @Patch()
+  patchTask() {
+    return this.tasksService.patchTask();
+  }
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,7 +7,21 @@ export class TasksService {
     return ['Task 1', 'Task 2', 'Task 3'];
   }
 
+  postTask() {
+    return 'POST a task';
+  }
+
+  putTask() {
+    return 'PUT a task';
+  }
+
+  deleteTask() {
+    return 'DELETE a task';
+  }
+ 
+  patchTask() {
+    return 'PATCH a task';
+  }
+
+
 }
-
-
-
