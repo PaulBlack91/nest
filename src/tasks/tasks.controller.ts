894ffe42a0a83653b8ea44df7ec7e702e6fa8212
Paul Esperon <paul.esperonarg@hotmail.com>
2024-07-09 23:59:35 -0300

    add task method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,7 +9,7 @@ export class TasksController {
   constructor(private tasksService: TasksService) { }
 
   @Get()
-  getAllTasks(@Query() Query: any) {
+  getAllTasks() {
     return this.tasksService.getTasks();
   }
 
