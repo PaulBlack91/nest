b5ec2dc12b722cf6110e2a497fcd805086f63797
Paul <paul.esperonarg@hotmail.com>
2024-07-16 20:33:28 -0300

    Add authentication middleware to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,19 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { LoggerMiddleware } from './logger/logger.middleware';
+import { AuthMiddleware } from './auth/auth.middleware';
+import path from 'path';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule implements NestModule{
+export class UsersModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('/users');
+    consumer
+      .apply(LoggerMiddleware).forRoutes('users')
+      .apply(AuthMiddleware).forRoutes({ path: 'users', method: RequestMethod.POST })
   }
 }
 
